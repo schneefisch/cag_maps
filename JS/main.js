1f0e1a6a36458dbe78fbc1a291754c5987a0afd8
Florian Röser <mail@schneefisch.de>
2015-11-30 12:01:27 +0100

    changes by ulf gerking

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+function Load(){
+    var loc = "HTML/Karte.html";
+    document.getElementById('iframe').setAttribute('src', loc);
+}
+function Load1(){
+    var loc1 = "HTML/Liste.html";
+    document.getElementById('iframe').setAttribute('src', loc1);
+}
+function Load2(){
+    var loc2 = "HTML/Developers.html";
+    document.getElementById('iframe').setAttribute('src', loc2);
+}
---
+++ JS/map.js
@@ -0,0 +1,25 @@
+var map;
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(48.778735, 9.178244),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+    // Marker aus der Liste
+    //crd1
+    var crd1 = new google.maps.LatLng(48.757743, 9.159263);
+    var marker1 = new google.maps.Marker({
+        position: crd1,
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
+    //crd2
+    var crd2 = new google.maps.LatLng();
+    var marker2 = new google.maps.Marker({
+        position: crd2,
+        map: map,
+        animation: google.maps.Animation.DROP
+    })
+    }
