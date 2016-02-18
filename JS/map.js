f487cba86f84edf45686da8ffbe6c82ba128217c
Florian Röser <mail@roeser-rombach.de>
2016-02-18 15:58:03 +0100

    funktion um die marker hinzuzufuegen

--- JS/map.js
+++ JS/map.js
@@ -2,15 +2,59 @@ var map;
 
 function initializeLeaflet() {
     // get the map and set the focus
-    map = L.map('map').setView([48.7772,9.1881], 14);
+    map = L.map('map').setView([48.7772,9.1881], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-
-    L.marker([48.7595,9.156939999999999]).addTo(map)
-    .bindPopup('<a target="_blank" href="http://www.stolpersteine-stuttgart.de/index.php?docid=748&mid=66">Gottlob Assenheimer <br> Finkenstr. 28</a><br> ')
 }
+
+function addMarkers(data) {
+    console.log("stolpersteine daten:");
+
+    // teile beim zeilenumbruch '\n'
+    var alleZeilen = data.split(/\r\n|\n/);
+    console.log(alleZeilen);
+
+    // titelzeile
+    var titel = alleZeilen[0].split(",");
+    console.log(titel);
+    var zeilen = [];
+
+    // hole alle Zeilen und teile beim komma
+    for (var i=1; i < alleZeilen.length; i++) {
+        var zeile_tmp = alleZeilen[i].split(',');
+
+        // pruefe ob die Zeile die gleiche laenge wie der Titel hat
+        if (zeile_tmp.length == titel.length) {
+            zeilen.push(zeile_tmp);
+        }
+    }
+
+    // fuege die Marker hinzu
+    for (var i=0; i < zeilen.length; i++) {
+        var zeile = zeilen[i];
+        var text = '<a target="_blank" href="' + zeile[2] + '">' + zeile[0] + '</a><br>' + zeile[1];
+        L.marker([zeile[3],zeile[4]]).addTo(map).bindPopup(text);
+    }
+}
+
+function loadCSV() {
+    $.ajax({
+        type: "GET",
+        url: "ressources/stolpersteine.csv",
+        dataType: "text",
+        success: function(data) {
+            addMarkers(data);
+        }
+    });
+}
+
+$(document).ready(function() {
+    initializeNavigation();
+    initializeLeaflet();
+    loadCSV();
+});
 
 
 //function initialize() {
@@ -40,4 +84,14 @@ function initializeLeaflet() {
 //        map: map,
 //        animation: google.maps.Animation.DROP
 //    })
+//}
+//
+
+
+
+//{"Ludwg Fleischer": {
+//        "strasse": "Reuchlinstr. 9",
+//        "url": "...",
+//        "lat": "asdf"
+//    }
 //}
