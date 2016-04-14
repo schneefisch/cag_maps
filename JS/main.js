e7ba7052d97f4953f142aa7ff394133b1c8a8868
Florian Röser <mail@roeser-rombach.de>
2016-04-14 16:35:32 +0200

    update main.js to add 'active' class when clicking on a button

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,16 @@
 // -------------------------
 var target;
 
-function loadPage(pageName) {
+function removeAllOtherActive() {
+    // todo
+}
+
+function loadPage(pageName, event) {
     target.empty();
     target.load(pageName);
     $("#bs-navbar").collapse('hide');
+    removeAllOtherActive();
+    $(event.target).addClass('active');
 }
 
 function initializeNavigation() {
@@ -15,28 +21,28 @@ function initializeNavigation() {
     target.load("HTML/map.html");
 
     // Home-button
-    $("#nav_home").click(function() {
-        loadPage("HTML/map.html");
+    $("#nav_home").click(function(event) {
+        loadPage("HTML/map.html", event);
     });
 
     // Steine Button
-    $("#nav_stones").click(function() {
-        loadPage("HTML/Liste.html");
+    $("#nav_stones").click(function(event) {
+        loadPage("HTML/Liste.html", event);
     });
 
     // Entwickler Button
-    $("#nav_developers").click(function() {
-        loadPage("HTML/Developers.html");
+    $("#nav_developers").click(function(event) {
+        loadPage("HTML/Developers.html", event);
     });
 
     // Impressum Button
-    $(".nav_impressum").click(function() {
-        loadPage("HTML/impressum.html");
+    $(".nav_impressum").click(function(event) {
+        loadPage("HTML/impressum.html", event);
     });
 
     // Kontakt Button
-    $(".nav_referenz").click(function() {
-        loadPage("HTML/datenschutz.html");
+    $(".nav_referenz").click(function(event) {
+        loadPage("HTML/datenschutz.html", event);
     });
 }
 
