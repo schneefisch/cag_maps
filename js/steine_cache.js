9b3b26943afb14f01685096046d4adc159226757
Florian Röser <mail@roeser-rombach.de>
2017-04-10 21:39:31 +0200

    update javascript to load data from CSV and store in browser session cache

---
+++ js/steine_cache.js
@@ -0,0 +1,14 @@
+
+
+var _steine = new Array();
+
+function put(stolperstein) {
+    steine.push(stolperstein)
+}
+
+function get(minLat, maxLat, minLng, maxLng) {
+
+    var steine = new Array();
+
+
+}
--- js/stolpersteine.js
+++ js/stolpersteine.js
@@ -2,6 +2,8 @@ var map;
 var latStuttgartZentrum = 48.7784485;
 var lngStuttgartZentrum = 9.1800132;
 var zoom = 15;
+var key = "st";
+var _stones;
 
 function renderMap() {
     console.log("start rendering map");
@@ -16,8 +18,66 @@ function renderMap() {
     }).addTo(map);
 }
 
+function loadStonesFromCSVCallback(stones) {
+    // add to browser cache
+    sessionStorage.setItem(key, stones);
+    _stones = stones;
+}
+
+function loadStonesFromCSV() {
+    $.ajax({
+        type: 'GET',
+        url: 'res/stolpersteine.csv',
+        dataType: 'text',
+        success: function(data) {
+            // parse data
+            var stones = {};
+            var allLines = data.split(/\r\n|\n/);
+            var expectedDataPerLine = 5;
+            // starting with i=1 because the first line (i=0) is the title line
+            for (var i=1; i<allLines.length; i++) {
+                var l = allLines[i].split(',');
+                if (l.lenght == expectedDataPerLine) {
+                    // when adding data, start with index 0
+                    stones[i-1] = {
+                        name: l[0],
+                        street: l[1],
+                        uri: l[2],
+                        lat: l[3],
+                        lng: l[4]
+                    }
+                }
+            }
+            // call callback method
+            loadStonesFromCSVCallback(stones);
+        }
+    });
+}
+
+function loadStonesFromCache() {
+
+    // load from browser-cache if existent
+    _stones = sessionStorage.getItem(key);
+
+    // if nothing is in browser-cache, load stones from csv and store in cache
+    if (_stones == null) {
+        loadStonesFromCSV();
+    }
+}
+
+function loadStones() {
+    if (Modernizr.localstorage) {
+        console.log("has local storage");
+        loadStonesFromCache();
+    } else {
+        console.log("no local storage available");
+        loadStonesFromCSV();
+    }
+}
+
 
 $(document).ready(function() {
     console.log("start rendering map");
     renderMap();
+    loadStones();
 });
